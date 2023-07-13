7ea185bda753212edb6861e08293d33f5cba8368
Levy <edgarlevy81@gmail.com>
2023-07-13 18:02:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(p,q) {
+    return p+q;
+}
+function subtract(p,q) {
+    return p-q;
+ }
+function multiply(p,q) {
+    return p*q;
+}
+function divide(p,q) {
+    return p/q;
+}
+function increment (n){
+    return n+1;
+}
+function decrement (n) {
+    return n-1;
+}
+function makeInt (dop){
+    return (parseInt(dop,10));
+}
+function preserveDecimal(drip){
+    return (parseFloat(drip));
+
+}
